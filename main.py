1ae05d6c79542291afd2f37300be1b9ac54f7b43
Mujtaba <mujtabah258@gmail.com>
2023-12-30 01:53:15 +0530

    Feature added: backend

---
+++ main.py
@@ -0,0 +1,98 @@
+import json
+import numpy as np
+import random
+from tensorflow import keras
+from keras.models import load_model
+import nltk
+from nltk.stem import WordNetLemmatizer
+from flask import Flask,request,jsonify
+app=Flask(__name__)
+
+nltk.download('punkt')
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
+
+model = load_model('chatbot_model.h5')
+
+with open('intents.json') as file:
+    data = json.load(file)
+# Load 'words' and 'classes' from file or define them here
+# Example: 
+# words = ['hello', 'goodbye', 'thanks', ...]
+# classes = ['greeting', 'farewell', 'gratitude', ...]
+lemmatizer = WordNetLemmatizer()
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!', '.', ',']
+
+
+# Iterate over the intents
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        # Tokenize each word
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        # Add to documents
+        documents.append((word_list, intent['tag']))
+        # Add to classes
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Lemmatize and lower each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+# Sort classes
+classes = sorted(list(set(classes)))
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence, words):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_intent(sentence, model, words):
+    bow = bag_of_words(sentence, words)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+# Example usage
+# flask app
+# @app.route('/',
+
+@app.route('/', methods=['GET'])
+def get_bot_response():
+    user_input = request.json['query']
+    intents_list = predict_intent(user_input, model, words)
+    response = get_response(intents_list, data)
+    return jsonify({'response': response})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
